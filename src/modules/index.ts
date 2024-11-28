3662df59c19c402b0cdcaeed7a2e12bd963fe959
Gabriel Guimarães <gabeguimaraes13@gmail.com>
2024-11-28 11:56:11 -0300

    feat: modules index

---
+++ src/modules/index.ts
@@ -0,0 +1,6 @@
+import { ExpensesModule } from './expenses/expenses.module';
+import { UsersModule } from './users/users.module';
+
+const modules = [UsersModule, ExpensesModule];
+
+export default modules;
